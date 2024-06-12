92ae163c321307992b1c20152ebe6b7a990e1863
GabrielHygino <gahygino@gmail.com>
2024-06-12 02:56:24 -0300

    add global guard

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,8 @@ import { PrismaService } from './prisma/prisma.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
   imports: [
@@ -12,6 +14,12 @@ import { ConfigModule } from '@nestjs/config';
     AuthModule,
     UsersModule,
   ],
-  providers: [PrismaService],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+    PrismaService,
+  ],
 })
 export class AppModule {}
